4cf9dbdfca08a69153479fb3daaa94002e7acb19
knknkn1162 <knknkn1162@gmail.com>
2024-01-02 09:08:48 +0900

    add event_propagation

---
+++ event_propagation/script.js
@@ -0,0 +1,17 @@
+function random(number) {
+  return Math.floor(Math.random()*number);
+}
+
+function bgChange() {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  return rndCol;
+}
+
+// bubble up div#container -> div(children)
+const container = document.querySelector('#container');
+container.addEventListener(
+  'click',
+  event => {
+    event.target.style.backgroundColor = bgChange()
+  }
+);
